4d6c6293ec3e68ac230bfe17dd99e86c7654a20c
Yan <yankaiquecosta.yk@gmail.com>
2019-11-17 22:04:35 -0300

    Deletando pastas execessivas e modificando rota de conhecer escola

--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,8 @@ import {
 } from "react-router-dom";
 
 import Home from './pages/index';
-import galeria from './pages/galeria';
-import atividades from './pages/atividades';
+import Galeria from './pages/galeria';
+import Atividades from './pages/atividades';
 import faq from './pages/faq';
 import cEscola from './pages/conhecendo_escola';
 import formulario from './pages/formulario';
@@ -17,10 +17,10 @@ export default function Routes(){
         <Router>
             <Switch>
                 <Route path="/" exact component={Home} />
-                <Route path="/galeria" component={galeria} />
-                <Route path="/atividades" component={atividades} />
+                <Route path="/galeria" component={Galeria} />
+                <Route path="/atividades" component={Atividades} />
                 <Route path="/faq" component={faq} />
-                <Route path="/conhecer_escola" component={cEscola} />
+                <Route path="/conhecer" component={cEscola} />
                 <Route path="/formulario" component={formulario} />
             </Switch>
         </Router>
